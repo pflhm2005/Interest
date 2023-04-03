95b4244c34219432feeec2a26e8bb87c4a7bac14
pflhm2005 <pangfeilong@szunicom.com>
2023-04-03 16:11:12 +0800

    mhxy

--- mhxy.js
+++ mhxy.js
@@ -1,36 +1,45 @@
 const sudo = require('sudo-prompt');
 
 async function closeHandle(cmd) {
-  return new Promise((resolve, reject) => {sudo.exec(cmd, { name: 'long'}, (err) => {
+  return new Promise((resolve, reject) => {sudo.exec(cmd, (err) => {
       if (err) reject(err);
       resolve();
     });
   });
 }
 
-sudo.exec('handle -a -vt -p mhmain', { name: 'long' }, function (err, stdout) {
-  if (err) console.log(err);
-  const ar = stdout.split('\n').filter(v => v).reduce((acc, cur) => {
-    if (cur.includes('BaseNamedObjects\\mhxy')) {
-      const arr = cur.split(/\s+/);
-      acc.push({
-        process: arr[0],
-        pid: arr[1],
-        user: arr[2],
-        handle: arr[3],
-        type: arr[4],
-        flags: arr[5],
-      });
-    }
-    return acc;
-  }, []);
-  const pid = ar[0].pid;
-  const cmds = ar.map(v => v.handle);
-  (async () => {
-    for(const cmd of cmds) {
-      console.log('正在处理handle' + cmd + '...');
-      await closeHandle(`handle -c ${cmd} -p ${pid} -y`);
-    }
-  })();
-  console.log('handle处理完毕');
-});
+function go() {
+  return new Promise((resolve, reject) => {
+    sudo.exec('handle -a -vt -p mhmain', function (err, stdout) {
+      if (err) reject(err);
+      const ar = stdout.split('\n').filter(v => v).reduce((acc, cur) => {
+        if (cur.includes('BaseNamedObjects\\mhxy')) {
+          const arr = cur.split(/\s+/);
+          acc.push({
+            process: arr[0],
+            pid: arr[1],
+            user: arr[2],
+            handle: arr[3],
+            type: arr[4],
+            flags: arr[5],
+          });
+        }
+        return acc;
+      }, []);
+      if (ar.length) {
+        const pid = ar[0].pid;
+        const cmds = ar.map(v => v.handle);
+        (async () => {
+          for(const cmd of cmds) {
+            console.log('正在处理handle' + cmd + '...');
+            await closeHandle(`handle -c ${cmd} -p ${pid} -y`);
+          }
+        })();
+        resolve('处理完毕');
+      }
+      reject('未找到对应进程');
+    });
+  });
+}
+
+go();
